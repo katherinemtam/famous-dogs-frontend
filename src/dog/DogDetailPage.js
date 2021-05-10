a4417579db575bc8d03d99a93a2c5bf294eb91c3
Katherine Tam <katherinemtam@gmail.com>
2021-05-10 14:33:00 -0700

    setup DogDetailPage

---
+++ src/dog/DogDetailPage.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './DogsDetailPage.css';
+
+export default class DogsDetailPage extends Component {
+
+  render() {
+
+    return (
+      <div className="Dog Detail">
+        <h2>Dog Detail Page</h2>
+
+      </div>
+    );
+  }
+
+}
