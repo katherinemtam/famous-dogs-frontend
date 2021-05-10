e6f2538f6d25cd06b8a9c5688fbc023da1dc3a59
Katherine Tam <katherinemtam@gmail.com>
2021-05-10 14:27:37 -0700

    setup header

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,18 +1,22 @@
 import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
 
-  render() { 
+  render() {
     return (
       <header className="Header">
 
         <h1>React App</h1>
-        
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/dogs">Dogs</NavLink>
+        </nav>
       </header>
     );
   }
 
 }
- 
+
 export default Header;
