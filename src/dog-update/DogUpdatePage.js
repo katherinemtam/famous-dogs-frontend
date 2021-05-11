f33699ff123369c6597987794aea2ac59c21cb57
Katherine Tam <katherinemtam@gmail.com>
2021-05-11 15:40:51 -0700

    add DogUpdatePage

---
+++ src/dog-update/DogUpdatePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import DogForm from '../common/DogForm';
+import './DogUpdatePage.css';
+
+class DogUpdatePage extends Component {
+  state = { 
+    dog: null,
+    loading: false
+  }
+
+  render() { 
+    const { dog } = this.state;
+    if (!dog) return null;
+
+    return ( 
+      <div className="DogEditPage">
+        <h2>Update {dog.name}</h2>
+        <DogForm dog={dog} onSubmit={this.handleUpdate}/>
+      </div>
+    );
+  }
+}
+ 
+export default DogUpdatePage;
