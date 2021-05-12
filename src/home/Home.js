82a73c57d36c8cbcc9f1b9ee1bec4f6aecdbc957
Katherine Tam <katherinemtam@gmail.com>
2021-05-12 10:12:43 -0700

    change theme

--- src/home/Home.js
+++ src/home/Home.js
@@ -10,13 +10,13 @@ export default class Home extends Component {
       <div className="Home">
         <div className="Wrapper">
           <header>
-            <h2>Welcome to Famous Dogs!</h2>
+            <h2>Welcome to the Famous Dogs Hotel!</h2>
             <h3>🐾Anything is paw-sible...🐾</h3>
           </header>
           <img src={homeGif} alt="dog's running"/>
           <h3>⭐The next dawg you see could be a star~⭐</h3>
           <p>
-            <Link to='/dogs'>List of Famous Doggos</Link>
+            <Link to='/dogs'>Famous Doggos Staying Here</Link>
           </p>
         </div>
       </div>
